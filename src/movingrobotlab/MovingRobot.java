0db3bd3cd089a4b90a7719a6a7b668090a832bb7
kasrasadeghi <kasra.sadeghi1@gmail.com>
2015-10-19 14:17:13 -0500

    0.1

--- src/movingrobotlab/MovingRobot.java
+++ src/movingrobotlab/MovingRobot.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this license header, choose License Headers in Project Properties;
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor;
  */
 package movingrobotlab;
 
@@ -14,20 +14,57 @@ import java.awt.Color;
 public class MovingRobot
 {
     // Declare instance variables here
+    private String name;
+    private int w;
+    private int h;
+    private Color torsoColor;
+    private Color legColor;
+    private boolean leftUp;
+    private boolean rightUp;
+    private double rightArmAngle;
+    private double leftArmAngle;
+    private int[] measures = new int[10];
     
-    
-    // Constructor: creates a robot with a name, width, height, and two colors
-    // inputs:
-    
-    
+    // Constructor: creates a robot with a name, w, h, and two colors
+    // inputs: String name, int w, int h, Color torsoColor, Color legColor
+    public MovingRobot(String name, int width, int height, Color torsoColor, Color legColor)
+    {
+        this.name = name;
+        this.w = width;
+        this.h = height;
+        this.torsoColor = torsoColor;
+        this.legColor = legColor;
+        
+        measures[0] = (4*h)/10;  //leg & torso height
+        measures[1] = h/10;      //pelvis and head height
+        measures[2] = (7*w)/10;  //shoulder width
+        measures[3] = (7*w)/20;  //waist width
+        measures[4] = (3*w)/20;  //arm width
+        measures[5] = h/2;       //arm height
+        measures[6] = (35*w)/300;//leg width
+        measures[7] = (3*w) / 10;//head width
+        measures[8] = (7*w)/30;  //name tag width
+        measures[9] = h/20;      //name tag height
+                
+    }   
     
     // getters -
     // provide a getter for each of the 5 supplied fields and each of the 4 other fields
+    public String getName()         { return name; }
+    public int getWidth()           { return w; }
+    public int getHeight()          { return h; }
+    public Color getTorsoColor()    { return torsoColor; }
+    public Color getLegColor()      { return legColor; }
     
+    public boolean isLeftUp()       { return leftUp; }
+    public boolean isRightUp()      { return rightUp; }
+    public double getRightArmAngle(){ return rightArmAngle; }
+    public double getLeftArmAngle() { return leftArmAngle; }
     
+    // THE GETTERSBURG ADDRESS
     // provide several other getters for convenience (there are 12 functions here)
+    public int[] getMeasures()      { return measures; }
     
-    
+    // THE SETTERS OF PLYMOUTH
     // raise/lower arms/legs functions
-    
 }
